fb3465ec94a132966ea7c56f4bc8f7694f4066cc
pasindu <madushan01722@gmail.com>
2023-11-03 10:21:24 +0530

    create properties & behaviours in LoginFormController class

--- src/main/java/lk/ijse/ProjectSihina/controller/LoginFormController.java
+++ src/main/java/lk/ijse/ProjectSihina/controller/LoginFormController.java
@@ -1,5 +1,25 @@
 package lk.ijse.ProjectSihina.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginFormController {
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
 
 }
