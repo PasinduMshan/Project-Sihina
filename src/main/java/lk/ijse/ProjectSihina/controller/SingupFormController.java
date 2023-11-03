4d31962be406a7cdd54a994a78df92e1c67006d9
pasindu <madushan01722@gmail.com>
2023-11-03 10:30:00 +0530

    create properties & behaviours in SingupFormController class

--- src/main/java/lk/ijse/ProjectSihina/controller/SingupFormController.java
+++ src/main/java/lk/ijse/ProjectSihina/controller/SingupFormController.java
@@ -1,4 +1,37 @@
 package lk.ijse.ProjectSihina.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class SingupFormController {
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtFirstName;
+
+    @FXML
+    private JFXTextField txtLastName;
+
+    @FXML
+    private JFXTextField txtNIC;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
 }
